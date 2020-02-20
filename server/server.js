68cb2598c0879f83c913b66dfa41d0a3330dcb8d
bmwoolf <bradleymwoolf@gmail.com>
2020-02-19 18:03:06 -0800

    add server to serve static assets

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+
+app.use(express.json());
+app.use(express.static('assets'));
+
+// serve the static assets
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '../index.html'));
+});
+
+
+
+// 404 handler
+app.use('*', (req, res) => {
+    res.sendStatus(404);
+  });
+
+// global error handler
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.sendStatus(500);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
