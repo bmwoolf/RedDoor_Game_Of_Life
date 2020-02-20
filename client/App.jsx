ebff767a108462ded766a65049b8afb49a4a1b29
bmwoolf <bradleymwoolf@gmail.com>
2020-02-19 18:02:40 -0800

    add boilerplate folders for assets, containers, components

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import React from  "react";
+import './assets/styles.scss';
+
+
+const App = () => {  
+    return (
+        <div>
+            <h1> hello from react </h1>
+        </div>
+      );
+}
+
+ 
+export default App;
---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import App from './App.jsx';
+import styles from './assets/styles.scss';
+// import { createRoot } from 'react-dom@experimental';
+
+
+render(<App />, document.getElementById("root"));
