ad8e454fc01d62b9bd0b6ee7603e9404baf25bf9
bmwoolf <bradleymwoolf@gmail.com>
2020-02-24 12:07:32 -0800

    change prop naming

--- client/components/Box.jsx
+++ client/components/Box.jsx
@@ -2,32 +2,12 @@ import React, { useEffect } from 'react';
 import Grid from './Grid.jsx';
 
 const Box = (props) => {
-    // let boxClass = props.boxClass;
-    // useEffect(() => {
-    //     console.log('inside useEffect')
-    //     if (props.value === 1) {
-    //         boxClass = 'on';
-    //     } else {
-    //         boxClass = 'off';
-    //     }
-    // }, [])
-    // create a local function to invoke colorBox() with multiple args
-    function color() {
-        console.log('color')
-        props.colorBox(props.row, props.column);
-        props.play2() // this cancels out the coloring functionality
-        console.log('boxClass', props.boxClass)
-    }
 
-    // check the value of the box, and we'll check the value here if 0 or 1, change it here
-    // props.value
-    // function checkNumber() {
-    //     if (props.value === 1) {
-    //         boxClass = 'on';
-    //     } else {
-    //         boxClass = 'off';
-    //     }
-    // }
+    // create a local function to invoke colorBox() with multiple arguments
+    function color() {
+        props.colorBox(props.row, props.column);
+        props.go();
+    };
 
     return ( 
         <div 
@@ -37,7 +17,7 @@ const Box = (props) => {
           onClick={() => color()}
         >
         </div>
-        );
+    );
 }
  
 export default Box;
