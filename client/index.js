992d3f11b35701c6efb671feeeb67a888ba75bcc
bmwoolf <bradleymwoolf@gmail.com>
2020-02-24 12:35:59 -0800

    format changes

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import App from './App.jsx';
 import styles from './assets/styles.scss';
-// import { createRoot } from 'react-dom@experimental';
 
 
 render(<App />, document.getElementById("root"));
