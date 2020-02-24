3ba1d620717d6670e72fe19960697d4583b212d4
bmwoolf <bradleymwoolf@gmail.com>
2020-02-24 12:52:17 -0800

    format changes

--- client/components/Grid.jsx
+++ client/components/Grid.jsx
@@ -29,38 +29,6 @@ const Grid = (props) => {
             {rowsArr}
         </div>
     );
-}
- 
-// class Grid extends Component {
-//     render(props) { 
-//         const width = props.columns * 14;
-//         let rowsArr = [];
-//     // change box naming
-//         let boxClass = "";
-//     //change to map method
-//         for (let i = 0; i < props.rows; i++) {
-//             for (let j = 0; j < props.columns; j++) {
-//                 let boxKey = `${i}-${j}`;
-//                 boxClass = props.entireGrid[i][j] ? 'box on' : 'box off';
-//                 rowsArr.push(
-//                     <Box 
-//                         boxClass={boxClass}
-//                         key={boxKey}
-//                         boxId={boxKey}
-//                         row={i}
-//                         column={j}
-//                         selectBox={props.selectBox}
-//                     />
-//                 );
-//             }
-//         }
-
-//         return ( 
-//             <div className="grid" style={{width: width}}> 
-//                 {rowsArr}
-//             </div>
-//          );
-//     }
-// }
+};
 
 export default Grid;
